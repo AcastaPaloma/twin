8419d4e6e6dbed2ed1c94ab9df9a626312e83399
BX <78887920+billxby@users.noreply.github.com>
2025-09-14 05:48:55 -0400

    delete stuff

--- supabase/edge_function_testing/functions/logger.ts
+++
@@ -1,6 +0,0 @@
-// Simple logger for frontend
-export function logResult(result: string) {
-  // Log to browser console
-  console.log("[AnalyzeAllUsers Result]", result);
-  // Optionally, you could show a toast or alert here
-}
